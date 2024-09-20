26608f5fda8eea55678e08100f94307a8939feb0
Tico <patricio.osterno1@gmail.com>
2024-09-20 03:05:53 -0300

    Componentes básicos criados

---
+++ psico-mara-site/src/app/components/home/home.component.ts
@@ -0,0 +1,44 @@
+import { Component } from '@angular/core';
+import {HeaderComponent} from "../../shared/header/header.component";
+import {Router} from "@angular/router";
+import {FooterComponent} from "../../shared/footer/footer.component";
+
+@Component({
+  selector: 'app-home',
+  standalone: true,
+  imports: [
+    HeaderComponent,
+    FooterComponent
+  ],
+  templateUrl: './home.component.html',
+  styleUrl: './home.component.scss'
+})
+export class HomeComponent {
+
+  constructor(private router: Router) {
+  }
+
+  navigateHome() {
+    this.router.navigate(["home"])
+  }
+
+  navigateAbout() {
+    this.router.navigate(["about"])
+  }
+
+  navigateServices() {
+    this.router.navigate(["services"])
+  }
+
+  navigateTestimonials() {
+    this.router.navigate(["testimonials"])
+  }
+
+  navigateContact() {
+    this.router.navigate(["contact"])
+  }
+
+  navigateBlog() {
+    this.router.navigate(["blog"])
+  }
+}
