c5445314e18f970155f8e3b9db5a4e03b48682e2
Tico <patricio.osterno1@gmail.com>
2024-09-23 12:24:58 -0300

    Construção da página "blog", agora "trg"

--- psico-mara-site/src/app/components/blog/blog.component.ts
+++ psico-mara-site/src/app/components/blog/blog.component.ts
@@ -1,12 +1,27 @@
 import { Component } from '@angular/core';
+import {HeaderComponent} from "../../shared/header/header.component";
+import {FooterComponent} from "../../shared/footer/footer.component";
+import {Router} from "@angular/router";
 
 @Component({
   selector: 'app-blog',
   standalone: true,
-  imports: [],
+  imports: [
+    HeaderComponent,
+    FooterComponent
+  ],
   templateUrl: './blog.component.html',
   styleUrl: './blog.component.scss'
 })
 export class BlogComponent {
 
+  constructor(private router:Router) {
+  }
+  navigateTestimonials(): void {
+    this.router.navigate(["testimonials"]);
+  }
+
+  navigateBlog(): void {
+    this.router.navigate(["blog"]);
+  }
 }
