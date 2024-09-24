3fcfd249f5d11dee12406e2932ec29a61fe08902
Tico <patricio.osterno1@gmail.com>
2024-09-23 21:31:46 -0300

    Inserido a base da página de depoimentos e corrigida alguns bugs no header

--- psico-mara-site/src/app/components/testimonials/testimonials.component.ts
+++ psico-mara-site/src/app/components/testimonials/testimonials.component.ts
@@ -1,12 +1,28 @@
 import { Component } from '@angular/core';
+import {HeaderComponent} from "../../shared/header/header.component";
+import {FooterComponent} from "../../shared/footer/footer.component";
+import {Router} from "@angular/router";
 
 @Component({
   selector: 'app-testimonials',
   standalone: true,
-  imports: [],
+  imports: [
+    HeaderComponent,
+    FooterComponent
+  ],
   templateUrl: './testimonials.component.html',
   styleUrl: './testimonials.component.scss'
 })
 export class TestimonialsComponent {
+
+  constructor(private router:Router) {
+  }
+  navigateTestimonials(): void {
+    this.router.navigate(["testimonials"]);
+  }
+
+  navigateBlog(): void {
+    this.router.navigate(["blog"]);
+  }
 
 }
